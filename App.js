25fe0204335cd1236e8a4fd513c4259bf14194e1
srivathsankr7 <srivathsankr7@gmail.com>
2023-12-04 13:44:50 +0530

    Clear all

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-const p = <p id="test">This is a paragraph</p>
-const heading = React.createElement("h1", {}, ["Hello from new react app!", p]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
